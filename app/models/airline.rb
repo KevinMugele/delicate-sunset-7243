86ed4b49a57112d4f8242a70c9602203c2207bb4
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-24 11:31:50 -0400

    Update airline model #adult_passengers to sort and order by most flights

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,9 +3,10 @@ class Airline < ApplicationRecord
 
   def adult_passengers
     flights.joins(flight_passengers: :passenger)
-           .select('passengers.*')
+           .select('passengers.*, count(flight_passengers.id) AS flight_count')
            .where('passengers.age >= ?', 18)
            .group('passengers.id')
+           .order('flight_count DESC')
            .distinct
   end
 end
