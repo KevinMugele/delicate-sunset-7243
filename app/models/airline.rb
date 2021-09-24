a418125f80bcf89147b869dff00a48cedbc0564b
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-24 11:16:38 -0400

    Write method to find passengers who are older than 18 and unique

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,11 @@
 class Airline < ApplicationRecord
   has_many :flights
+
+  def adult_passengers
+    flights.joins(flight_passengers: :passenger)
+           .select('passengers.*')
+           .where('passengers.age >= ?', 18)
+           .group('passengers.id')
+           .distinct
+  end
 end
