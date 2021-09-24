c801b84438603f8d3b49573d23c1dd4dc7ac117b
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-24 10:59:48 -0400

    Write tests for removing a passenger

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -8,14 +8,13 @@ RSpec.describe 'Flights Index Page' do
     @flight2 = @airline1.flights.create!(number: 2, date: "10/24/21", departure_city: "JFK", arrival_city: "Denver")
     @flight3 = @airline1.flights.create!(number: 3, date: "9/23/21", departure_city: "DC", arrival_city: "Seattle")
     @passenger1 = Passenger.create!(name: "Kevin", age: 30)
-    @passenger2 = Passenger.create!(name: "Henry", age: 70)
     @passenger3 = Passenger.create!(name: "Khoi", age: 60)
     @passenger4 = Passenger.create!(name: "Andrew", age: 50)
     @passenger5 = Passenger.create!(name: "Kelsey", age: 40)
     @passenger6 = Passenger.create!(name: "Laura", age: 20)
 
     @pf1 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight1.id)
-    @pf2 = FlightPassenger.create!(passenger_id: @passenger2.id, flight_id: @flight1.id)
+
     @pf3 = FlightPassenger.create!(passenger_id: @passenger3.id, flight_id: @flight2.id)
     @pf4 = FlightPassenger.create!(passenger_id: @passenger4.id, flight_id: @flight2.id)
     @pf5 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
@@ -26,12 +25,12 @@ RSpec.describe 'Flights Index Page' do
 
   describe 'user story 1: Flights Index Page' do
     it 'shows all flight numbers' do
-      save_and_open_page
+      # save_and_open_page
       within("#flight-#{@flight1.id}") do
         expect(page).to have_content(@flight1.number)
         expect(page).to have_content(@airline1.name)
         expect(page).to have_content(@passenger1.name)
-        expect(page).to have_content(@passenger2.name)
+
       end
 
       within("#flight-#{@flight2.id}") do
@@ -47,6 +46,18 @@ RSpec.describe 'Flights Index Page' do
         expect(page).to have_content(@passenger5.name)
         expect(page).to have_content(@passenger6.name)
       end
+    end
+  end
+
+  describe 'user story 2: Remove a Passenger From a Flight' do
+    it 'shows a link to remove a passenger from a flight' do
+      within("#flight-#{@flight1.id}") do
+        expect(page).to have_content(@passenger1.name)
+        expect(page).to have_link("Remove Passenger")
+        click_link("Remove Passenger")
+      end
+      expect(current_path).to eq(flights_path)
+      expect(page).to_not have_content(@passenger1.name)
     end
   end
 end
