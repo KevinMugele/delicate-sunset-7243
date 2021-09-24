33cf1f908c9d602ca6000efb1e69efd2a7ba1e98
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-24 11:17:02 -0400

    Create show_spec for airlines. Added test for user story 3

---
+++ spec/features/airlines/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Airlines Showw Page' do
+  before :each do
+    @airline1 = Airline.create!(name: "American Airlines")
+    @flight1 = @airline1.flights.create!(number: 1, date: "10/23/21", departure_city: "Newark", arrival_city: "LA")
+    @flight2 = @airline1.flights.create!(number: 2, date: "10/24/21", departure_city: "JFK", arrival_city: "Denver")
+    @flight3 = @airline1.flights.create!(number: 3, date: "9/23/21", departure_city: "DC", arrival_city: "Seattle")
+    @passenger1 = Passenger.create!(name: "Kevin", age: 30)
+    @passenger2 = Passenger.create!(name: "Khoi", age: 6)
+    @passenger3 = Passenger.create!(name: "Andrew", age: 5)
+    @passenger4 = Passenger.create!(name: "Kelsey", age: 40)
+    @passenger5 = Passenger.create!(name: "Laura", age: 18)
+
+    @pf1 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight1.id)
+    @pf2 = FlightPassenger.create!(passenger_id: @passenger3.id, flight_id: @flight2.id)
+    @pf3 = FlightPassenger.create!(passenger_id: @passenger4.id, flight_id: @flight2.id)
+    @pf4 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
+    @pf5 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
+    @pf6 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight3.id)
+
+    visit airline_path(@airline1)
+  end
+
+  describe 'user story 3: Airlines passenger' do
+    it 'shows all passengers on the airlines flights that are older than 18 and unique' do
+      expect(page).to have_content(@passenger1.name)
+      expect(page).to have_content(@passenger4.name)
+      expect(page).to have_content(@passenger5.name)
+      expect(page).to_not have_content(@passenger2.name)
+      expect(page).to_not have_content(@passenger3.name)
+    end
+  end
+end
