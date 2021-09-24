3597a7768f4b4d2df50214e97d2026d449a630bc
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-24 11:32:20 -0400

    Write tests for #adult_passengers in both features and models

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -15,9 +15,11 @@ RSpec.describe 'Airlines Showw Page' do
     @pf1 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight1.id)
     @pf2 = FlightPassenger.create!(passenger_id: @passenger3.id, flight_id: @flight2.id)
     @pf3 = FlightPassenger.create!(passenger_id: @passenger4.id, flight_id: @flight2.id)
-    @pf4 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
+    @pf4 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight1.id)
     @pf5 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
     @pf6 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight3.id)
+    @pf7 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight2.id)
+    @pf8 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
 
     visit airline_path(@airline1)
   end
@@ -29,6 +31,13 @@ RSpec.describe 'Airlines Showw Page' do
       expect(page).to have_content(@passenger5.name)
       expect(page).to_not have_content(@passenger2.name)
       expect(page).to_not have_content(@passenger3.name)
+    end
+  end
+
+  describe 'Extension: Frequent Flyers' do
+    it 'should show flyers who have the most flights sorted most to least' do
+      expect(@passenger5.name).to appear_before(@passenger1.name)
+      expect(@passenger1.name).to appear_before(@passenger4.name)
     end
   end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -4,4 +4,33 @@ RSpec.describe Airline do
   describe 'relationships' do
     it { should have_many(:flights) }
   end
+
+  it '#adult_passengers' do
+    @airline1 = Airline.create!(name: "American Airlines")
+    @flight1 = @airline1.flights.create!(number: 1, date: "10/23/21", departure_city: "Newark", arrival_city: "LA")
+    @flight2 = @airline1.flights.create!(number: 2, date: "10/24/21", departure_city: "JFK", arrival_city: "Denver")
+    @flight3 = @airline1.flights.create!(number: 3, date: "9/23/21", departure_city: "DC", arrival_city: "Seattle")
+    @passenger1 = Passenger.create!(name: "Kevin", age: 30)
+    @passenger2 = Passenger.create!(name: "Khoi", age: 6)
+    @passenger3 = Passenger.create!(name: "Andrew", age: 5)
+    @passenger4 = Passenger.create!(name: "Kelsey", age: 40)
+    @passenger5 = Passenger.create!(name: "Laura", age: 18)
+
+    @pf1 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight1.id)
+    @pf2 = FlightPassenger.create!(passenger_id: @passenger3.id, flight_id: @flight2.id)
+    @pf3 = FlightPassenger.create!(passenger_id: @passenger4.id, flight_id: @flight2.id)
+    @pf4 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight1.id)
+    @pf5 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
+    @pf6 = FlightPassenger.create!(passenger_id: @passenger1.id, flight_id: @flight3.id)
+    @pf7 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight2.id)
+    @pf8 = FlightPassenger.create!(passenger_id: @passenger5.id, flight_id: @flight3.id)
+
+    actual = @airline1.adult_passengers.map do |passenger|
+      passenger.id
+    end
+
+    expected = [@passenger5.id, @passenger1.id, @passenger4.id]
+
+    expect(actual).to eq(expected)
+  end
 end
